35570ca1824fe046a89bfec542aaa44d57ef3532
tmoatmoa <982735884@qq.com>
2021-09-14 13:54:52 +0800

    9.14  13：54

--- refresh_c++/main.cpp
+++ refresh_c++/main.cpp
@@ -34,24 +34,47 @@ vector<int> runningSum(vector<int>& nums) {
 }
 
 int main() {
-//    vector<int> a = {8,4,6,2,3};
-//    vector<int> b;
-//    cout<<"aa"<<a.size()<<endl;
-//    b = runningSum(a);
-//    for(int i=0;i<b.size();i++)
-//        cout<<b[i]<<endl;
+    auto a=L'a';
+    auto b=0x4d;
+    auto c="a";
+    auto d=L"a";
+    cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl;
 
-    std::string str ("This is an example sentence.");
-    std::cout << str << '\n';
-    // "This is an example sentence."
-    str.erase (10,8);                        //            ^^^^^^^^
-    std::cout << str << '\n';
-    // "This is an sentence."
-    str.erase (str.begin()+9);               //           ^
-    std::cout << str << '\n';
-    // "This is a sentence."
-    str.erase (str.begin()+5, str.end()-9);  //       ^^^^^
-    std::cout << str << '\n';
-    // "This sentence."
+//    int sum=0,val; //p15 1.16
+//    for(int i=0;i<5;i++){
+//        cin>>val;
+//        sum+=val;
+//    }
+//    cout<<sum<<endl;
+
+//    int sum=0; //p12 1.12
+//    for(int i=-100;i<=100;++i){
+//        sum+=i;
+//    }
+//    cout<<sum<<endl;
+
+//    int num1,num2; // p11 1.11
+//    cout<<"input"<<endl;
+//    cin>>num1>>num2;
+//    int i = num1<num2 ?num1:num2;
+//    while(i<=(num1>num2?num1:num2)){
+//        cout<<i<<endl;
+//        i++;
+//    }
+
+//    for(int i=10;i>0;i--){//p11 1.10
+//        cout<<i<<endl;
+//    }
+//    int i=50,sum=0; //p11 1.9
+//    while(i<100){
+//        sum+=i;
+//        i++;
+//    }
+//    cout<<sum<<endl;
+
+//    cout<<"/*"; //p9 1.8
+//    cout<<"*/";
+//    cout<</*"*/" */;
+//    cout<</* "*/" /* "/*" */;
     return 0;
 }
